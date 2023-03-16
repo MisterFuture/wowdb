b89cb7a3306c78327b37565ef0fe0993b5f216f9
Guillermo-Fernandez-Gonzalez <guillermo.ossu@proton.me>
2023-03-16 21:04:16 +1100

    fixed minor typos

--- src/pages/html_utils.tsx
+++ src/pages/html_utils.tsx
@@ -3,7 +3,7 @@ import Head from 'next/head'
 export default function htmlHead(title: string, icon: string = "/favicon.ico", description: string = ""){
     return(
       <Head>
-          <title>title</title>
+          <title>{title}</title>
           <meta name="description" content= {description} />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="icon" href= {icon} />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      {htmlHead("Create Next App")}
+      {htmlHead("Create Next.js App")}
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
